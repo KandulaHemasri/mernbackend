e06496ae9023c1f71d5ee98ad2f777756503467d
KandulaHemasri <hemasrikandula801@gmail.com>
2025-07-10 10:46:55 +0530

    done

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./routes/userRoute.js";
+
+
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(express.static("public"));
 
 
